f9bbf2e24c73479ded7922142ea6aa9c6e6f63f5
COOVI DOSSOU-KOKO <bdossoukoko@gmail.com>
2020-07-15 19:53:04 -0400

    Updates

---
+++ precache-manifest.04ce90ab316232a21dcfb8cf25b82ad3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f9ab324b6760fd78457b1edcb8fd606",
+    "url": "/restaurant/index.html"
+  },
+  {
+    "revision": "6493c7b24ad0d18ef17a",
+    "url": "/restaurant/static/css/main.04e04f6a.chunk.css"
+  },
+  {
+    "revision": "ff9277a5740ff291f4cc",
+    "url": "/restaurant/static/js/2.50636072.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/restaurant/static/js/2.50636072.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6493c7b24ad0d18ef17a",
+    "url": "/restaurant/static/js/main.26eef168.chunk.js"
+  },
+  {
+    "revision": "745b7a2b6ee8e92dc73a",
+    "url": "/restaurant/static/js/runtime-main.414afd18.js"
+  }
+]);
