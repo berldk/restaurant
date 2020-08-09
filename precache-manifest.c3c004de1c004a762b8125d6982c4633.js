a0c4f172c61479844b984e5dfac2984943b24cec
berldk <58774067+berldk@users.noreply.github.com>
2020-08-09 13:32:49 -0400

    Updates

--- precache-manifest.c3c004de1c004a762b8125d6982c4633.js
+++ precache-manifest.c3c004de1c004a762b8125d6982c4633.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "26854363793d965635b0981922425fcd",
+    "revision": "94620a29a4db3f0789eeef22d3b82e27",
     "url": "/alamaison/index.html"
   },
   {
-    "revision": "1c737b3e288cbc2d40e4",
+    "revision": "3dc77d29b10de34c1bae",
     "url": "/alamaison/static/css/main.04e04f6a.chunk.css"
   },
   {
-    "revision": "0bb352332c0a8f0d406f",
-    "url": "/alamaison/static/js/2.9f4619ad.chunk.js"
+    "revision": "ff9277a5740ff291f4cc",
+    "url": "/alamaison/static/js/2.50636072.chunk.js"
   },
   {
     "revision": "77e7f328dacc981fca681ee4ae94d669",
-    "url": "/alamaison/static/js/2.9f4619ad.chunk.js.LICENSE.txt"
+    "url": "/alamaison/static/js/2.50636072.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1c737b3e288cbc2d40e4",
-    "url": "/alamaison/static/js/main.9885f65b.chunk.js"
+    "revision": "3dc77d29b10de34c1bae",
+    "url": "/alamaison/static/js/main.80c90972.chunk.js"
   },
   {
     "revision": "6da23b1649424bb10cc0",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/alamaison/precache-manifest.3ed3d89a30a374de1ed7496b04305fda.js"
+  "/alamaison/precache-manifest.c3c004de1c004a762b8125d6982c4633.js"
 );
 
 self.addEventListener('message', (event) => {
